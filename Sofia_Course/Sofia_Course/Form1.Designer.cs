3a517fb463d2cf4f3204f0d94bab457f423f6654
vittrum <viktorkovalskyi9@gmail.com>
2019-12-10 21:39:40 +0200

    proper_init

---
+++ Sofia_Course/Sofia_Course/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Sofia_Course {
+    partial class Form1 {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent() {
+            this.TabControl = new System.Windows.Forms.TabControl();
+            this.LoginPage = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbPass = new System.Windows.Forms.TextBox();
+            this.tbLogin = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboRoles = new System.Windows.Forms.ComboBox();
+            this.TabControl.SuspendLayout();
+            this.LoginPage.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TabControl
+            // 
+            this.TabControl.Controls.Add(this.LoginPage);
+            this.TabControl.Controls.Add(this.tabPage2);
+            this.TabControl.Location = new System.Drawing.Point(12, 12);
+            this.TabControl.Name = "TabControl";
+            this.TabControl.SelectedIndex = 0;
+            this.TabControl.Size = new System.Drawing.Size(933, 521);
+            this.TabControl.TabIndex = 0;
+            // 
+            // LoginPage
+            // 
+            this.LoginPage.Controls.Add(this.comboRoles);
+            this.LoginPage.Controls.Add(this.label3);
+            this.LoginPage.Controls.Add(this.label2);
+            this.LoginPage.Controls.Add(this.tbLogin);
+            this.LoginPage.Controls.Add(this.tbPass);
+            this.LoginPage.Controls.Add(this.label1);
+            this.LoginPage.Controls.Add(this.btnLogin);
+            this.LoginPage.Location = new System.Drawing.Point(4, 26);
+            this.LoginPage.Name = "LoginPage";
+            this.LoginPage.Padding = new System.Windows.Forms.Padding(3);
+            this.LoginPage.Size = new System.Drawing.Size(925, 491);
+            this.LoginPage.TabIndex = 0;
+            this.LoginPage.Text = "Login";
+            this.LoginPage.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 26);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(925, 491);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(358, 283);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(100, 38);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "Войти";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(296, 146);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Логин";
+            // 
+            // tbPass
+            // 
+            this.tbPass.Location = new System.Drawing.Point(358, 177);
+            this.tbPass.Name = "tbPass";
+            this.tbPass.Size = new System.Drawing.Size(100, 25);
+            this.tbPass.TabIndex = 2;
+            // 
+            // tbLogin
+            // 
+            this.tbLogin.Location = new System.Drawing.Point(358, 146);
+            this.tbLogin.Name = "tbLogin";
+            this.tbLogin.Size = new System.Drawing.Size(100, 25);
+            this.tbLogin.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(295, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Пароль";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(305, 217);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 19);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Роль";
+            // 
+            // comboRoles
+            // 
+            this.comboRoles.FormattingEnabled = true;
+            this.comboRoles.Location = new System.Drawing.Point(358, 211);
+            this.comboRoles.Name = "comboRoles";
+            this.comboRoles.Size = new System.Drawing.Size(121, 25);
+            this.comboRoles.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(957, 545);
+            this.Controls.Add(this.TabControl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TabControl.ResumeLayout(false);
+            this.LoginPage.ResumeLayout(false);
+            this.LoginPage.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl TabControl;
+        private System.Windows.Forms.TabPage LoginPage;
+        private System.Windows.Forms.ComboBox comboRoles;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbLogin;
+        private System.Windows.Forms.TextBox tbPass;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
+
